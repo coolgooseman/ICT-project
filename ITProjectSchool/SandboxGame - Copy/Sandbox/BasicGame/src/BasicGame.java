78b1828c82951c5a75fa966793fcdcba93463d98
Jorn <71011796+coolgooseman@users.noreply.github.com>
2022-12-09 12:40:36 +0100

    Initial commit

---
+++ ITProjectSchool/SandboxGame - Copy/Sandbox/BasicGame/src/BasicGame.java
@@ -0,0 +1,132 @@
+import nl.saxion.app.SaxionApp;
+import nl.saxion.app.interaction.GameLoop;
+import nl.saxion.app.interaction.KeyboardEvent;
+import nl.saxion.app.interaction.MouseEvent;
+
+
+public class BasicGame implements GameLoop {
+    boolean optie1 = false;
+    boolean optie2 = false;
+    boolean optie3 = false;
+    String currentScreen = "startPagina";
+
+    public static void main(String[] args) {
+        SaxionApp.startGameLoop(new BasicGame(), 750, 750, 40);
+    }
+
+    @Override
+    public void init() {
+
+
+    }
+
+    @Override
+    public void loop() {
+        SaxionApp.clear();
+        switch (currentScreen) {
+            case "gameMenu" -> gameMenu();
+            case "startPagina" -> startPagina();
+            case "gamePagina" -> gamePagina();
+            case "regelPagina" -> regelPagina();
+        }
+        
+    }
+
+    @Override
+    public void keyboardEvent(KeyboardEvent keyboardEvent) {
+    }
+
+    @Override
+    public void mouseEvent(MouseEvent mouseEvent) {
+        if(mouseEvent.isMouseUp() && mouseEvent.isLeftMouseButton()){
+            if(optie1 || optie2 || optie3)
+            {
+                dobbelsteen();
+            }
+        }
+
+        if (currentScreen.equals("startPagina")) {
+            if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                //int x = mouseEvent.getX();
+                //int y = mouseEvent.getY();
+                currentScreen = "gameMenu";
+            }
+        } else if (currentScreen.equals("gameMenu")) {
+            if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                int x = mouseEvent.getX();
+                int y = mouseEvent.getY();
+                if (y < 198 && y > 94) {
+                    if (x > 0 && x < 485) {
+                        currentScreen = "gamePagina";
+                        optie1 = true;
+                    }
+                }
+            }
+            if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                int x = mouseEvent.getX();
+                int y = mouseEvent.getY();
+                if (y < 338 && y > 235) {
+                    if (x > 0 && x < 485) {
+                        currentScreen = "gamePagina";
+                        optie2 = true;
+                    }
+                }
+            }
+            if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                int x = mouseEvent.getX();
+                int y = mouseEvent.getY();
+                if (y < 480 && y > 375) {
+                    if (x > 0 && x < 485) {
+                        currentScreen = "gamePagina";
+                        optie3 = true;
+                    }
+                }
+            }
+            if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                int x = mouseEvent.getX();
+                int y = mouseEvent.getY();
+                if (y < 620 && y > 517) {
+                    if (x > 0 && x < 485) {
+                        currentScreen = "regelPagina";
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+
+    public void dobbelsteen(){
+        int randomNummer = SaxionApp.getRandomValueBetween(1,7);
+        switch (randomNummer) {
+            case 1 -> SaxionApp.drawImage("stip1.png", 350, 345, 60, 60);
+            case 2 -> SaxionApp.drawImage("stip2.png", 350, 345, 60, 60);
+            case 3 -> SaxionApp.drawImage("stip3.png", 350, 345, 60, 60);
+            case 4 -> SaxionApp.drawImage("stip4.png", 350, 345, 60, 60);
+            case 5 -> SaxionApp.drawImage("stip5.png", 350, 345, 60, 60);
+            case 6 -> SaxionApp.drawImage("stip6.png", 350, 345, 60, 60);
+        }
+    }
+
+    public void gameMenu() {
+        SaxionApp.drawImage("keuze menu.png", 0, 0, 750, 750);
+    }
+
+    public void startPagina() {
+        SaxionApp.drawImage("start pagina.png", 0, 0, 750, 750);
+
+    }
+
+    public void gamePagina() {
+
+        SaxionApp.drawImage("bord.png", 0, 0, 750, 750);
+    }
+
+    public void regelPagina() {
+        //SaxionApp.drawimage("foto.png",0,0,750,750);
+        //wachten op foto collin denk ik
+    }
+}
+
---
+++ ITProjectSchool/SandboxGame - Copy/Sandbox/BasicGame/src/Players.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Players {
+    Color kleur = Color.BLACK;
+    int x;
+    int y;
+    int id;
+    String naam = "";
+}
