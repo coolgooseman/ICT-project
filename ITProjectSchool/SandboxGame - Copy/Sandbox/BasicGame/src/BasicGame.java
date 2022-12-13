bbd61764b0d7b3c2c96db485d465a145507fe334
Jorn <71011796+coolgooseman@users.noreply.github.com>
2022-12-13 20:51:41 +0100

    Playermovment initialize // logic

--- ITProjectSchool/SandboxGame - Copy/Sandbox/BasicGame/src/BasicGame.java
+++ ITProjectSchool/SandboxGame - Copy/Sandbox/BasicGame/src/BasicGame.java
@@ -1,23 +1,20 @@
-import nl.saxion.app.CsvReader;
 import nl.saxion.app.SaxionApp;
 import nl.saxion.app.interaction.GameLoop;
 import nl.saxion.app.interaction.KeyboardEvent;
 import nl.saxion.app.interaction.MouseEvent;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 
 public class BasicGame implements GameLoop {
-    boolean mensNiet = false;
-    boolean verzuipNiet = false;
-    boolean mensWel = false;
+    boolean optie1 = false;
+    boolean optie2 = false;
+    boolean optie3 = false;
     String currentScreen = "startPagina";
     int counter = -1;
-    int aantalSpelers = 0;
+    int randomNummer = 0;
 
-    //BoardPositions[] positie = new BoardPositions[56];
-    ArrayList<Player> players = new ArrayList<>();
+    BoardPositions[] positie = new BoardPositions[56];
 
     public static void main(String[] args) {
         SaxionApp.startGameLoop(new BasicGame(), 750, 750, 40);
@@ -25,12 +22,13 @@ public class BasicGame implements GameLoop {
 
     @Override
     public void init() {
+        playerMovement();
 
     }
 
     @Override
     public void loop() {
-        SaxionApp.clear();
+        //SaxionApp.clear();
         switch (currentScreen) {
             case "startPagina" -> startPagina();
             case "gameMenu" -> gameMenu();
@@ -38,9 +36,6 @@ public class BasicGame implements GameLoop {
             case "mensWel" -> welGamePagina();
             case "verzuipNiet" -> drankGamePagina();
             case "regelPagina" -> regelPagina();
-            case "mensNietRegels" -> mensNietRegels();
-            case "verzuipNietRegels" -> verzuipNietRegels();
-            case "mensWelRegels" -> mensWelRegels();
             case "playerMenu" -> playerMenu();
             case "2player" -> twoPlayer();
             case "3player" -> threePlayer();
@@ -58,6 +53,8 @@ public class BasicGame implements GameLoop {
         switch (currentScreen) {
             case "startPagina":
                 if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
+                    //int x = mouseEvent.getX();
+                    //int y = mouseEvent.getY();
                     currentScreen = "gameMenu";
                 }
                 break;
@@ -67,8 +64,8 @@ public class BasicGame implements GameLoop {
                     int y = mouseEvent.getY();
                     if (y < 198 && y > 94) {
                         if (x > 0 && x < 485) {
-                            currentScreen = "playerMenu";
-                            mensNiet = true;
+                            currentScreen = "mensNiet";
+                            optie1 = true;
                         }
                     }
                 }
@@ -77,8 +74,8 @@ public class BasicGame implements GameLoop {
                     int y = mouseEvent.getY();
                     if (y < 338 && y > 235) {
                         if (x > 0 && x < 485) {
-                            currentScreen = "playerMenu";
-                            verzuipNiet = true;
+                            currentScreen = "verzuipNiet";
+                            optie2 = true;
                         }
                     }
                 }
@@ -87,8 +84,8 @@ public class BasicGame implements GameLoop {
                     int y = mouseEvent.getY();
                     if (y < 480 && y > 375) {
                         if (x > 0 && x < 485) {
-                            currentScreen = "playerMenu";
-                            mensWel = true;
+                            currentScreen = "mensWel";
+                            optie3 = true;
                         }
                     }
                 }
@@ -100,6 +97,14 @@ public class BasicGame implements GameLoop {
                             currentScreen = "regelPagina";
                         }
                     }
+                }
+                break;
+            case "mensNiet":
+            case "verzuipNiet":
+            case "mensWel":
+                currentScreen = "playerMenu";
+                if (mouseEvent.isMouseUp() && mouseEvent.isLeftMouseButton()) {
+                    dobbelsteen();
                 }
                 break;
             case "playerMenu":
@@ -130,171 +135,62 @@ public class BasicGame implements GameLoop {
                         }
                     }
                 }
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 620 && y > 517) {
-                        if (x > 0 && x < 485) {
-                            currentScreen = "gameMenu";
-                        }
-                    }
-                }
-                break;
-            case "regelPagina":
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 283 && y > 179) {
-                        if (x > 0 && x < 528) {
-                            currentScreen = "mensNietRegels";
-                        }
-                    }
-                }
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 424  && y > 320) {
-                        if (x > 0 && x < 528) {
-                            currentScreen = "verzuipNietRegels";
-                        }
-                    }
-                }
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 565 && y > 461) {
-                        if (x > 0 && x < 528) {
-                            currentScreen = "mensWelRegels";
-                        }
-                    }
-                }
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 750 && y > 654) {
-                        if (x > 0 && x < 534) {
-                            currentScreen = "gameMenu";
-                        }
-                    }
-                }
-                break;
-            case "mensNietRegels":
-            case "verzuipNietRegels":
-            case "mensWelRegels":
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 750 && y > 654) {
-                        if (x > 0 && x < 534) {
-                            currentScreen = "regelPagina";
-                        }
-                    }
-                }
-                break;
-            case "mensNiet":
-            case "verzuipNiet":
-            case "mensWel":
-                if (mouseEvent.isMouseDown() && mouseEvent.isLeftMouseButton()) {
-                    int x = mouseEvent.getX();
-                    int y = mouseEvent.getY();
-                    if (y < 750 && y > 715) {
-                        if (x > 0 && x < 750) {
-                            currentScreen = "gameMenu";
-                        }
-                    }
-                }
-                if (mouseEvent.isMouseUp() && mouseEvent.isLeftMouseButton()) {
-                    dobbelsteen();
-                }
-                break;
         }
 
     }
 
     public void playerMenu(){
-        SaxionApp.drawImage("Sandbox/player menu.png",0,0,750,750);
+        /*
+        SaxionApp.drawImage("keuze menu.png",0,0,750,750);
+        //SaxionApp.drawRectangle(200,200, 20,20);
+        int aantalSpelers = 0;
+        switch (aantalSpelers){
+            case 2: //SaxionApp.drawImage('...',0,0,750,750); break;
+            case 3: //SaxionApp.drawImage('...',0,0,750,750); break;
+            case 4: //SaxionApp.drawImage('...',0,0,750,750); break;
+        }
+
+        ArrayList<Players> players = new ArrayList<>();
+
+         */
     }
 
     public void twoPlayer(){
-        aantalSpelers = 2;
-        if(mensNiet){
+        //SaxionApp.drawImage("...",0,0,750,750);
+        if(optie1){
             currentScreen = "mensNiet";
-        } else if (verzuipNiet) {
+        } else if (optie2) {
             currentScreen = "verzuipNiet";
-        } else if (mensWel){
+        } else if (optie3){
             currentScreen = "mensWel";
         }
+
     }
 
     public void threePlayer(){
-        aantalSpelers = 3;
-        if(mensNiet){
+        //SaxionApp.drawImage("...",0,0,750,750);
+        if(optie1){
             currentScreen = "mensNiet";
-        } else if (verzuipNiet) {
+        } else if (optie2) {
             currentScreen = "verzuipNiet";
-        } else if (mensWel){
+        } else if (optie3){
             currentScreen = "mensWel";
         }
     }
 
     public void fourPlayer(){
-        aantalSpelers = 4;
-        if(mensNiet){
+        //SaxionApp.drawImage("...",0,0,750,750);
+        if(optie1){
             currentScreen = "mensNiet";
-        } else if (verzuipNiet) {
+        } else if (optie2) {
             currentScreen = "verzuipNiet";
-        } else if (mensWel){
+        } else if (optie3){
             currentScreen = "mensWel";
         }
     }
 
-    public void startPagina() {
-        SaxionApp.drawImage("Sandbox/start pagina.png", 0, 0, 750, 750);
-    }
-
-    public void gameMenu() {
-        SaxionApp.drawImage("Sandbox/keuze menu.png", 0, 0, 750, 750);
-    }
-
-    public void nietGamePagina() {
-        SaxionApp.drawImage("Sandbox/bord mens erger je niet.png", 0, 0, 750, 750);
-
-        readPlayersIn();
-    }
-
-    public void drankGamePagina() {
-        SaxionApp.drawImage("Sandbox/bord mens erger je niet.png", 0, 0, 750, 750);
-
-        readPlayersIn();
-    }
-
-    public void welGamePagina() {
-        SaxionApp.drawImage("Sandbox/bord mens erger je wel.png", 0, 0, 750, 750);
-
-        readPlayersIn();
-    }
-
-    public void regelPagina() {
-        SaxionApp.drawImage("Sandbox/regels keuze menu.png",0,0,750,750);
-    }
-
-    public void mensNietRegels(){
-        SaxionApp.drawImage("Sandbox/regels.png",0,0,750,750);
-        SaxionApp.drawText("mens erger je niet",200,200,30);
-    }
-
-    public void verzuipNietRegels(){
-        SaxionApp.drawImage("Sandbox/regels.png",0,0,750,750);
-        SaxionApp.drawText("mens verzuip je niet",200,200,30);
-    }
-
-    public void mensWelRegels(){
-        SaxionApp.drawImage("Sandbox/regels.png",0,0,750,750);
-        SaxionApp.drawText("mens erger je wel",200,200,30);
-    }
-
     public void dobbelsteen() {
-        int randomNummer = SaxionApp.getRandomValueBetween(1, 7);
+        randomNummer = SaxionApp.getRandomValueBetween(1, 7);
         switch (randomNummer) {
             case 1 -> SaxionApp.drawImage("Sandbox/stip1.png", 350, 348, 55, 55);
             case 2 -> SaxionApp.drawImage("Sandbox/stip2.png", 350, 348, 55, 55);
@@ -305,96 +201,35 @@ public class BasicGame implements GameLoop {
         }
     }
 
-    public void readPlayersIn(){
+    public void startPagina() {
+        SaxionApp.drawImage("Sandbox/start pagina.png", 0, 0, 750, 750);
 
-        CsvReader reader = new CsvReader("Sandbox/pionPositie.csv");
-        reader.skipRow();
-        reader.setSeparator(';');
+    }
 
-        while(reader.loadRow()){
-            //color, x, y, h, w, id
-            Player p = new Player();
-            p.kleur = reader.getString(0);
-            p.x = reader.getInt(1);
-            p.y = reader.getInt(2);
-            p.h = reader.getInt(3);
-            p.w = reader.getInt(4);
-            p.id = reader.getInt(5);
+    public void gameMenu() {
+        SaxionApp.drawImage("Sandbox/keuze menu.png", 0, 0, 750, 750);
+    }
 
-            players.add(p);
-        }
+    public void nietGamePagina() {
 
-        for (Player p : players) {
-            switch (aantalSpelers) {
-                case 2:
-                    switch (p.kleur) {
-                        case "red" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.red);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "yellow" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.yellow);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "green", "blue" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.gray);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                    }
-                    break;
-                case 3:
-                    switch (p.kleur) {
-                        case "red" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.red);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "yellow" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.yellow);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "green" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.green);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "blue" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.gray);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                    }
-                    break;
-                case 4:
-                    switch (p.kleur) {
-                        case "red" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.red);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "yellow" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.yellow);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "green" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.green);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                        case "blue" -> {
-                            SaxionApp.turnBorderOff();
-                            SaxionApp.setFill(Color.blue);
-                            SaxionApp.drawRectangle(p.x, p.y, p.w, p.h);
-                        }
-                    }
-                    break;
-            }
-        }
+        SaxionApp.drawImage("Sandbox/bord mens erger je niet.png", 0, 0, 750, 750);
+        playerMenu();
+    }
+
+    public void welGamePagina() {
+
+        SaxionApp.drawImage("Sandbox/bord mens erger je niet.png", 0, 0, 750, 750);
+    }
+
+    public void drankGamePagina() {
+
+        SaxionApp.drawImage("Sandbox/bord mens erger je wel.png", 0, 0, 750, 750);
+        playerMenu();
+    }
+
+    public void regelPagina() {
+        //SaxionApp.drawimage("foto.png",0,0,750,750);
+        //wachten op foto collin denk ik
     }
 
     public void playerMovement() {
@@ -405,6 +240,7 @@ public class BasicGame implements GameLoop {
             pos.x = 315;
             pos.y = p;
             pos.position = counter;
+            positie[counter] = pos;
             SaxionApp.drawRectangle(pos.x, pos.y, 20,20);
         }
         //left
@@ -414,6 +250,7 @@ public class BasicGame implements GameLoop {
             pos.x = p;
             pos.y = 410;
             pos.position = counter;
+            positie[counter] = pos;
             SaxionApp.drawRectangle(pos.x, pos.y, 20,20);
         }
         //up
@@ -423,6 +260,7 @@ public class BasicGame implements GameLoop {
             pos.x = 70;
             pos.y = p;
             pos.position = counter;
+            positie[counter] = pos;
             SaxionApp.drawRectangle(pos.x, pos.y, 20,20);
         }
         //right
@@ -505,6 +343,11 @@ public class BasicGame implements GameLoop {
             pos.y = 670;
             pos.position = counter;
             SaxionApp.drawRectangle(pos.x, pos.y, 20,20);
+        }
+    }
+    public void actualPlayerMovement(){
+        for (int i = 0; i < randomNummer; i++){
+
         }
     }
 }
